72b4520399bd78e85f31cb596cf85fe0ecf69e6b
Izabela <izabela.tarasin@gmail.com>
2024-09-02 14:56:51 +0200

    created empty project

---
+++ src/main/java/com/izabela/peakslibrary/PeakslibraryApplication.java
@@ -0,0 +1,13 @@
+package com.izabela.peakslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakslibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakslibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izabela/peakslibrary/PeakslibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izabela.peakslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakslibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
